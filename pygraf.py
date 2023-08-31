5b6709006bdb43b6bed23f404b3a87dc9ea6230f
ilyaloban <ilyalobanov09@outlook.com>
2023-08-31 19:16:22 +0700

    Error solved and updated README.md

--- pygraf.py
+++ pygraf.py
@@ -1,58 +1,53 @@
 x = 0.0
-	y = 0.0
-	
-	
-	#код
-	import turtle
-	import math
-	import time
-	
-	
-	
-	t = turtle
-	t.ht()
-	t.speed(10)
-	
-	t.pu()
-	
-	def screen_conf(title="Graph", height=500, width=500):
-		sc = t.Screen()
-		sc.setup(height, width)
-		
-		t.title(title)
-		
-	def screen_save():
-		turtle.done()
+y = 0.0
+
+
+#код
+import turtle
+import math
+import time
+
+t = turtle
+t.ht()
+t.speed(10)
+
+t.pu()
+
+def screen_conf(title="Graph", height=500, width=500):
+	sc = t.Screen()
+	sc.setup(height, width)
+
+	t.title(title)
+
+def screen_save():
+	turtle.done()
 	
 	#координатная плоскость
-	def draw_coord(size=500):
-		ft = ("Arial", 4, "normal")
-		
-		t.goto(-size, 0)
-		t.write(str(-size), font=ft)
-		t.pd()
-		t.goto(size, 0)
-		t.write(str(size), font=ft)
-		t.pu()
-		
-		t.goto(0, -size)
-		t.write(str(-size), font=ft)
-		t.pd()
-		t.goto(0, size)
-		t.write(str(size), font=ft)
-		t.pu()
+def draw_coord(size=500):
+	ft = ("Arial", 4, "normal")
+
+	t.goto(-size, 0)
+	t.write(str(-size), font=ft)
+	t.pd()
+	t.goto(size, 0)
+	t.write(str(size), font=ft)
+	t.pu()
+
+	t.goto(0, -size)
+	t.write(str(-size), font=ft)
+	t.pd()
+	t.goto(0, size)
+	t.write(str(size), font=ft)
+	t.pu()
 	# y = ... equation graph
-	class y_plot(object):
-		def __init__(self, equation, mult=1,  x_from = -100, x_to = 100):
-			self.equation = equation
-			self.x_from = x_from
-			self.x_to = x_to
-			self.mult = mult
-		
-		
-	
-			
-		def draw(self, color):
+class y_plot(object):
+	def __init__(self, equation, mult=1,  x_from = -100, x_to = 100):
+		self.equation = equation
+		self.x_from = x_from
+		self.x_to = x_to
+		self.mult = mult
+
+	def draw(self, color):
 			#рисование графика
 			t.color(color)
 			try:
@@ -72,28 +67,28 @@ x = 0.0
 			t.pu()
 	
 	#custom graph
-	class list_plot(object):
-		def __init__(self, data_array, start_x = 0, start_y = 0):
-			self.da = data_array
-			self.start_x = start_x
-			self.start_y = start_y
+class list_plot(object):
+	def __init__(self, data_array, start_x = 0, start_y = 0):
+		self.da = data_array
+		self.start_x = start_x
+		self.start_y = start_y
 			
 		
-		def draw(self, color):
-			if (len(self.da) != 0):
-				pass
-			else:
-				raise Exception("Data from array not found.")
-			t.pu()
-			t.color(color)
-			t.goto(self.start_x, self.da[0] + self.start_y)
-			t.pd()
-			sx = self.start_x
-			sy = self.start_y
-			for i in range(sx, len(self.da) + sx):
-				x = sx + i
-				y = self.da[i - sx] + sy
-				t.goto(i, y)
-			t.pu()
+	def draw(self, color):
+		if (len(self.da) != 0):
+			pass
+		else:
+			raise Exception("Data from array not found.")
+		t.pu()
+		t.color(color)
+		t.goto(self.start_x, self.da[0] + self.start_y)
+		t.pd()
+		sx = self.start_x
+		sy = self.start_y
+		for i in range(sx, len(self.da) + sx):
+			x = sx + i
+			y = self.da[i - sx] + sy
+			t.goto(i, y)
+		t.pu()
 			
 		
