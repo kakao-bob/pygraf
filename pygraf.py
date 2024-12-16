05dff3e229a22d9110d2691b707b7188538eb05d
ilyaloban <ilyalobanov09@outlook.com>
2024-12-16 21:56:29 +0700

    Error solved and updated README.md

--- pygraf.py
+++ pygraf.py
@@ -1,29 +1,69 @@
 x = 0.0
 y = 0.0
 
+upscale = 5
+disp_x = [-upscale*100, -upscale*100]
+disp_y = [upscale*100, upscale*100]
 
-#код
+
 import turtle
 import math
 import time
 
+
 t = turtle
+t.setworldcoordinates(disp_x[0], disp_x[1], disp_y[0], disp_y[1])
 t.ht()
 t.speed(10)
-
 t.pu()
 
-def screen_conf(title="Graph", height=500, width=500):
+def screen_conf(title: str="Graph", height: int=500, width:int=500, anim:bool=False):
+	"""
+    Настройка окна
+    :param title: Заголовок окна
+    :param height: Высота окна
+    :param width: Ширина окна
+    :param anim: Включить анимацию отрисовки?
+    :return: None
+	"""
 	sc = t.Screen()
+	if not anim: sc.tracer(0)
 	sc.setup(height, width)
 
 	t.title(title)
 
 def screen_save():
+	"""
+	Не закрывать окно и начать отслеживать перемещение.
+	:return: None
+	"""
+
+	def move(d1:list[int], d2:list[int], size=False):
+		global disp_x, disp_y, upscale
+		if size: upscale -= d1[0] / 100
+		if (upscale > 0) or not size:
+			disp_x[0] += d1[0]; disp_x[1] += d1[1]
+			disp_y[0] += d2[0]; disp_y[1] += d2[1]
+			t.setworldcoordinates(disp_x[0], disp_x[1], disp_y[0], disp_y[1])
+		else:
+			if size: upscale = 1
+
+
+	turtle.listen()
+	turtle.onkey(lambda: move([-100, -100], [100, 100], True), "-" ) # -, уменьшение
+	turtle.onkey(lambda: move([100, 100], [-100, -100], True), "=") # +, приближение
+	turtle.onkey(lambda: move([0, 100], [0, 100]), "Up")
+	turtle.onkey(lambda: move([0, -100], [0, -100]), "Down")
+	turtle.onkey(lambda: move([-100, 0], [-100, 0]), "Left")
+	turtle.onkey(lambda: move([100, 0], [100, 0]), "Right")
 	turtle.done()
-	
-	#координатная плоскость
-def draw_coord(size=500):
+
+def draw_coord(size:int=500):
+	"""
+	Отрисовать координатную плоскость
+	:param size: Размер
+	:return: None
+	"""
 	ft = ("Arial", 4, "normal")
 
 	t.goto(-size, 0)
@@ -41,14 +81,25 @@ def draw_coord(size=500):
 	t.pu()
 	# y = ... equation graph
 class y_plot(object):
-	def __init__(self, equation, mult=1,  x_from = -100, x_to = 100):
+	def __init__(self, equation:str, mult:float|int=1,  x_from:int = -100, x_to:int = 100):
+		"""
+		График зависимости Y от X
+		:param equation: математическое уравнение ('y = ..x..')
+		:param mult: домножение каждого значения Y на указанную величину (>=0.0.2)
+		:param x_from: Ограничение графика 'слева'
+		:param x_to: Ограничение графика 'справа'
+		"""
 		self.equation = equation
 		self.x_from = x_from
 		self.x_to = x_to
 		self.mult = mult
 
-	def draw(self, color):
-			#рисование графика
+	def draw(self, color:str="red"):
+			"""
+			Отрисовать график
+			:param color: Цвет
+			:return: None
+			"""
 			t.color(color)
 			try:
 				exec(self.equation)
@@ -68,17 +119,27 @@ class y_plot(object):
 	
 	#custom graph
 class list_plot(object):
-	def __init__(self, data_array, start_x = 0, start_y = 0):
+	def __init__(self, data_array:list[int], start_x:int = 0, start_y:int = 0):
+		"""
+		График значений списка
+		:param data_array: список
+		:param start_x: Сдвиг по X
+		:param start_y: Сдвиг по Y
+		"""
 		self.da = data_array
 		self.start_x = start_x
 		self.start_y = start_y
 			
 		
-	def draw(self, color):
-		if (len(self.da) != 0):
-			pass
-		else:
+	def draw(self, color:str="red"):
+		"""
+		Отрисовать график
+		:param color: Цвет
+		:return: None
+		"""
+		if len(self.da) == 0:
 			raise Exception("Data from array not found.")
+
 		t.pu()
 		t.color(color)
 		t.goto(self.start_x, self.da[0] + self.start_y)
@@ -90,5 +151,3 @@ class list_plot(object):
 			y = self.da[i - sx] + sy
 			t.goto(i, y)
 		t.pu()
-			
-		
